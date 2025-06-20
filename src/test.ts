8142cda38ebf13629c2c2925020c3b2da0dc61d8
jsmdevs <joacocomelgaxd@gmail.com>
2025-06-20 11:11:44 -0300

    Se añadió el test para la clase User

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,6 @@
 import test from "ava";
-import { Product } from "./index";
+import { Product, User } from "./index";
+
 
 test("Testeo la clase Product", (t) => {
   // test Product class
@@ -7,4 +8,13 @@ test("Testeo la clase Product", (t) => {
   t.is(product.name, "Product 1");
   t.is(product.price, 10);
 });
-test("Testeo la clase User y sus métodos", (t) => {});
+
+test("Testeo la clase User y sus métodos", (t) => {
+  const product = new Product("Nuevo producto", 1500);
+   const user = new User('Joaquin');
+   user.addProduct(product);
+
+   t.is(user.name, 'Joaquin');
+   t.is(user.products[0].name, 'Nuevo producto');
+   t.is(user.products[0].price, 1500);
+});
